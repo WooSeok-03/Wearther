8bbb4bf0bfd3f9ec251ec1a10c6f3861e46d5b41
WooSeok-03 <7458dntjr@gmail.com>
2022-10-21 02:54:55 +0900

    [COMMENT] 주석 추가 및 \n(줄바꿈) 추가

--- app/src/main/java/com/android/wearther/presentation/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/android/wearther/presentation/viewmodel/WeatherViewModel.kt
@@ -92,6 +92,7 @@ class WeatherViewModel(private val application: Application): ViewModel(){
                     val windValue = response.body()?.wind?.speed
                     windData.postValue(windValue.toString() + "m/s")
 
+                    // Current Weather Icon
                     val weatherIconValue = when(weather.value) {
                         "뇌우" -> R.drawable.icon_thunder
                         "이슬비" -> R.drawable.icon_drizzle
@@ -136,22 +137,22 @@ class WeatherViewModel(private val application: Application): ViewModel(){
     fun recommendDress(currentTemperature: Int) : String {
         lateinit var clothes: String
 
-        if(currentTemperature >= 27) {               // 27℃ ~
-            clothes = "민소매, 반바지, 반팔, 치마, 원피스"
-        } else if (currentTemperature in 23..26) { // 23℃ ~ 26℃
-            clothes = "반팔, 얇은 셔츠, 반바지, 면바지"
+        if(currentTemperature >= 28) {               // 28℃ ~
+            clothes = "민소매, 반바지, 반팔, \n치마, 원피스"
+        } else if (currentTemperature in 23..27) { // 23℃ ~ 27℃
+            clothes = "반팔, 얇은 셔츠, \n반바지, 면바지"
         } else if (currentTemperature in 20..22) { // 20℃ ~ 22℃
-            clothes = "긴팔, 얇은 가디건, 면바지, 청바지"
+            clothes = "긴팔, 얇은 가디건, \n면바지, 청바지"
         } else if (currentTemperature in 17..19) { // 17℃ ~ 19℃
-            clothes = "얇은 니트, 얇은 재킷, 가디건, 맨투맨, 면바지, 청바지"
+            clothes = "얇은 니트, 얇은 재킷, \n가디건, 맨투맨, 면바지, 청바지"
         } else if (currentTemperature in 12..16) { // 12℃ ~ 16℃
-            clothes = "얇은 재킷, 가디건, 야상, 맨투맨, 니트, 청바지, 면바지, 살구색 스타킹"
+            clothes = "얇은 재킷, 가디건, 야상, 맨투맨, \n니트, 청바지, 면바지, 살구색 스타킹"
         } else if (currentTemperature in 9..11) {  // 9℃ ~ 11℃
-            clothes = "재킷, 트랜치 코트, 야상, 니트, 면바지, 청바지, 검은색 스타킹"
+            clothes = "재킷, 트랜치 코트, 야상, \n니트, 면바지, 청바지, 검은색 스타킹"
         } else if (currentTemperature in 5..8) {   // 5℃ ~ 8℃
-            clothes = "코트, 가죽 재킷, 니트, 스카프, 두꺼운 바지, 히트텍"
+            clothes = "코트, 가죽 재킷, 니트, \n스카프, 두꺼운 바지, 히트텍"
         } else {                                // ~ 4℃
-            clothes = "패딩, 두꺼운 코트, 목도리, 기모 제품"
+            clothes = "패딩, 두꺼운 코트, \n목도리, 기모 제품"
         }
 
         return clothes
