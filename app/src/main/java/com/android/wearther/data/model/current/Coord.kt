8d718123f72a2c0d058b62d8b424cf70a9acd751
WooSeok-03 <7458dntjr@gmail.com>
2022-11-05 19:42:35 +0900

    [REFACTOR] Data Class

--- app/src/main/java/com/android/wearther/data/model/current/Coord.kt
+++ app/src/main/java/com/android/wearther/data/model/current/Coord.kt
@@ -1,6 +1,6 @@
 package com.android.wearther.data.model.current
 
 data class Coord(
-    val lat: Int,
+    val lat: Double,
     val lon: Double
 )
--- app/src/main/java/com/android/wearther/data/model/current/WeatherInfo.kt
+++ app/src/main/java/com/android/wearther/data/model/current/WeatherInfo.kt
@@ -1,7 +1,9 @@
 package com.android.wearther.data.model.current
 
+import com.android.wearther.data.model.WeatherConditions
+
 data class WeatherInfo(
-    val weather: String,
+    val weather: WeatherConditions,
     val temperature: Double,
     val wear: String,
     val humidity: Int,
